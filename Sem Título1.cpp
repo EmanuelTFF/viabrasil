1d26b35c083fe93201ab06b8f332ad91aef12354
Emanuel Tonis Florz Filho <emanuel.filho1@alunos.sc.senac.br>
2023-05-11 19:39:54 -0300

    site de skate

---
+++ Sem Título1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int const TAM = 5;
+int i,num,qtde_neg = 0;
+main()
+{
+  system("chcp 65001"); //para usar acentuação
+  cout<<"\n\n\t\t ****  PESQUISA DE NÚMEROS    *** \n\n";
+  for (i=1;i<10;i++)
+  { cout<<"\n Informe o "<<i<<"º número:";
+    cin>>num;
+    if (num<0)//verifica se o número é ímpar
+    {  qtde_neg++; //contador
+    }
+  }
+  cout<<"\n Quantidade de números negativos informados: "<<qtde_neg;
+  cout<<"\n\n\n\n\n"; 
+}
+
